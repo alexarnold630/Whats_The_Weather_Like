af70e22d7a7df5a12b0909a60030b5848716e20c
Alex Arnold <alexarnold630@gmail.com>
2021-04-14 14:29:45 -0500

    readme

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "ENTER YOUR KEY HERE"
+weather_api_key = "0e37546ab98b973bf5b6e9d85aa6ec63"
 
 # Google API Key
-g_key = "ENTER YOUR KEY HERE"
+g_key = "AIzaSyBvXV0uII3_546jPPXZ91JRl4bmNVfXxZo"
